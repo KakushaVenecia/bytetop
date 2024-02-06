749dfca12d0e93746cd31470d9d61e968678aab1
KakushaVenecia <kakushavenecia@gmail.com>
2024-02-06 07:35:35 +0000

    made blade templates for navbar and preliminary footer

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <img class="logo" src="images/Logo.png" alt="Logo">
+
+    <div class="search-container">
+        <input type="text" id="searchInput" placeholder="Search...">
+        <button onclick="performSearch()">Search</button>
+    </div>
+    <div id="searchResults"></div>
+    <div class="nav-links">
+        <a href="#">About</a>
+        <a href="#">Sign Up</a>
+        <a href="#">Log In</a>
+        <a href="#">Basket</a>
+    </div>
+</nav>
