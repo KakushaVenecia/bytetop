f75e90915e4d638786a09addc3f0b105420859d5
ManeendraReddy <230342404@aston.ac.uk>
2024-02-08 10:38:47 +0000

    Login page and the password icon to be placed

---
+++ resources/views/forgotpwd.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Forgot Password</title>
+<link rel="stylesheet" type="text/css" href="css/forgotpwd.css">
+</head>
+<body>
+
+<div class="container">
+	<h2>Forgot</h2>
+	<h3>Password</h3>
+    <hr class="white-line">
+
+	<p>That's ok, It happens!</p>
+	<p>Enter your email address</p>
+	<input type="text" id="email" name="email" placeholder="Email address">
+	<button>Continue</button><br/><br/>
+</div>
+
+</body>
+</html>
