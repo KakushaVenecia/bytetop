6d5f5b1fb7d73f6d85a2f327bbc57a4f2ad0cd5d
ManeendraReddy <230342404@aston.ac.uk>
2024-02-09 11:33:28 +0000

    Features
    This will add the checkmail.php and login files

---
+++ resources/views/checkmail.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Forgot Password</title>
+<link rel="stylesheet" type="text/css" href="css/checkmail.css">
+</head>
+<body>
+
+<div class="container">
+	<h2>Check your e-mail</h2>
+    <hr class="white-line">
+
+	<p>Your forgot password request submitted</p>
+	<p>We’ve sent an email to usermail@gmail.com to reset your password and dive into your account. The link in the email will expire in 24 hours.</p>
+  <a href="#">Click here</a> - not received an email / like to change the email address
+
+	
+</div>
+<script src="js/forgotpwd.js"></script>
+</body>
+</html>
