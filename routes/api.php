3a626a7c572b72497281925b8515bb09345bb046
Muh76 <Mj.babaie@gmail.com>
2024-02-25 23:00:22 +0000

    Product CRUD

--- routes/api.php
+++ routes/api.php
@@ -34,7 +34,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
 
-Route::patch('/products', [ProductController::class, 'edit'])->name('');
+Route::patch('/products/{id}', [ProductController::class, 'update'])->name('products.update');
 
 Route::get('/allproducts', [ProductController::class, 'index']);
 
