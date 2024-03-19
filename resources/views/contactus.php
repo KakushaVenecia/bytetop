6bbbcbc127bc9bd0629297198098e65c2e9d824b
Saikiran_SurineniLogin. <saikiransurineni616@gmail.com>
2024-03-19 23:07:22 +0000

    Contactus page is done

--- resources/views/contactus.php
+++ resources/views/contactus.php
@@ -1,106 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/contactus.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Us</title>
+  <link rel="stylesheet" href="css/contactus.css">
 </head>
 <body>
-    
-<section id="contact" class="contact">
-      <div class="container">
+  <h3>Contact Us</h3>
 
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="info-box mb-4">
-              <i class="bx bx-map"></i>
-              <h3>India</h3>
-              <p><b>Bengaluru :</b> 90/10, VRS Complex, BTS Layout, B.G Road, Bengaluru - 560076</p>
-			  <p><b>Andhra Pradesh :</b> 11-464, 1 st lane, Panduranga Nagar, Guntur - 522034</p>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-envelope"></i>
-              <h3>Email Us</h3>
-              <p>info@perasiatech.com</p>
-			  <p>www.perasiatech.com</p>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-phone-call"></i>
-              <h3>Call Us</h3>
-              <p>+91 (855) 3912 070 <br> +91 (854) 6992 070</p>
-            </div>
-          </div>
-		  <div class="col-lg-6">
-            <div class="info-box mb-4">
-              <i class="bx bx-map"></i>
-              <h3>USA</h3>
-			  <p>Perasia Technologies LLC</p>
-              <p>2222 W. Grand River Ave Ste A Okemos, MI 48864</p>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-envelope"></i>
-              <h3>Email Us</h3>
-              <p>info@perasiatech.com</p>
-			  <p>www.perasiatech.com</p>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box  mb-4">
-              <i class="bx bx-phone-call"></i>
-              <h3>Call Us</h3>
-              <p>+1 (206) 778 4497</p>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-6 ">
-		  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.3263981792697!2d77.6065879737501!3d12.886721316736171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6b260eb27929%3A0x1389df094d18b78c!2sPerasia%20Technologies%20Private%20Limited!5e0!3m2!1sen!2sin!4v1681967612688!5m2!1sen!2sin" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div>
-
-          <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required="">
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required="">
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required=""></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div>
-
-        </div>
-
+  <div class="container">
+    <form action="/action_page.php">
+      <div class="form-group">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Enter Your name.." required>
       </div>
-    </section>
 
+      <div class="form-group">
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Enter Your last name.." required>
+      </div>
 
+      <div class="form-group">
+        <label for="Eid">Email ID</label>
+        <input type="email" id="Eid" name="Emailid" placeholder="Enter Your Email ID.." required>
+      </div>
+
+      <div class="form-group">
+        <label for="Cnumber">Contact Number</label>
+        <input type="tel" id="Cnumber" name="Contact Number" placeholder="Enter Your Contact Number.." required>
+      </div>
+
+      <div class="form-group">
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" required></textarea>
+      </div>
+
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 </body>
 </html>
