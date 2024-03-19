0144de467d73e54f9bfb6692375f6ad8eb09fcb1
Morikeh <230140888@aston.ac.uk>
2024-03-19 17:27:53 +0000

    payments page

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -7,4 +7,21 @@ use Illuminate\Http\Request;
 class PaymentController extends Controller
 {
     //
+    
+
+    public function store(Request $request)
+    { dd($request);
+        // Validate request data
+        $validatedData = $request->validate([
+            'card_number' => 'required',
+            'expiry_date' => 'required',
+            'security_code' => 'required',
+            'name' => 'required',
+        ]);
+
+        // Create a new shipping address
+        $payment = Payment::create($validatedData);
+
+       
+    }
 }
