033dc5e8dc2ee075a7daa3e6fcf78f8fdd33fe67
Saikiran_SurineniLogin. <saikiransurineni616@gmail.com>
2024-03-19 16:07:22 +0000

    Navbar drop down

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,18 @@
-<link rel="stylesheet" href="css/nav.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/nav.css">
+    <title>Navigation</title>
+</head>
+<body>
 
 <nav>
-    <div class="logo" href="{{ route('landing') }}"> 
-        <img class="logo" src="/images/Logo.png" alt="Logo">
+    <div class="logo">
+        <a href="{{ route('landing') }}">
+            <img class="logo" src="/images/Logo.png" alt="Logo">
+        </a>
     </div>
     
     <div class="search-container">
@@ -13,14 +23,19 @@
     </div>
     
     <div class="nav-links"> 
-        <a href="/products">Products</a>
-        {{-- <a href="/cartpage"> Cart <i class="fas fa-shopping-cart"></i> </a> --}}
-        {{-- <span id="cartCount"> --}}
-            {{-- <span id="cartCount">{{ $cartCount }}< </span> --}}
-        <a href="/cartpage"> Cart <i class="fas fa-shopping-cart"></i> </a>
+        <div class="dropdown">
+            <a href="/products" class="dropbtn">Products</a>
+            <div class="dropdown-content">
+                <a href="/laptops">Laptops</a>
+                <a href="/pcs">Computers</a>
+                <a href="/accessories">Accessories</a>
+            </div>
+        </div>
+        <a href="/cartpage">Cart <i class="fas fa-shopping-cart"></i></a>
+        
         @auth
             <div class="dropdown">
-                <a class="navbutton">Hello, {{ auth()->user()->name }} </a>
+                <a class="navbutton">Hello, {{ auth()->user()->name }}</a>
                 <div class="dropdown-content">
                     <div class="dropdown-links">
                         <a href="/account"><i class="fas fa-user"></i> My Account</a>
@@ -44,4 +59,3 @@
 <script src="js/nav.js"></script>
 </body>
 </html>
-
