f819d23c2b097635be2a500cd3bf557b1de0a507
ManeendraReddy <230342404@aston.ac.uk>
2024-03-20 13:43:48 +0000

    Trackorder page is done

---
+++ resources/views/trackorder.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/trackorder.css">
+</head>
+<body>
+<div class="container">
+<div class="d-flex">
+                <h5>ORDER <span class="text-primary font-weight-bold">#R86F854G7665</span></h5>
+            </div>
+            <div class="d-flex flex-column text-sm-right">
+                <p class="mb-0">Expected Arrival <span>02 April 2024</span></p>
+            </div>
+  <div class="row">
+						<div class="col-12 col-md-10 hh-grayBox pt45 pb20">
+							<div class="row justify-content-between">
+
+								<div class="order-tracking completed">
+									<span class="is-complete"></span>
+									<p><span>Mon, March 25</span></p>
+                                    <div class="row d-flex icon-content">
+                                        <img class="icon" src="/images/orderprocessed.png">
+                                        <div class="d-flex flex-column">
+                                            <p class="font-weight-bold">Order<br>Processed</p>
+                                        </div>
+                                    </div>
+								</div>
+
+								<div class="order-tracking completed">
+									<span class="is-complete"></span>
+									<p><span>Thu, March 28</span></p>
+                                    <div class="row d-flex icon-content">
+                                        <img class="icon" src="/images/ordershipped.png">
+                                        <div class="d-flex flex-column">
+                                            <p class="font-weight-bold">Order<br>Shipped</p>
+                                        </div>
+                                    </div>
+								</div>
+
+
+                                <div class="order-tracking">
+									<span class="is-complete"></span>
+									<p><span>Sat, March 30</span></p>
+                                    <div class="row d-flex icon-content">
+                                        <img class="icon" src="/images/enroute.png">
+                                        <div class="d-flex flex-column">
+                                            <p class="font-weight-bold">Order<br>En Route</p>
+                                        </div>
+                                    </div>
+								</div>
+
+
+								<div class="order-tracking">
+									<span class="is-complete"></span>
+									<p><span>Tue, April 2</span></p>
+                                    <div class="row d-flex icon-content">
+                                        <img class="icon" src="/images/home.png">
+                                        <div class="d-flex flex-column">
+                                            <p class="font-weight-bold">Order<br>Arrived</p>
+                                        </div>
+                                    </div>
+								</div>
+							</div>
+						</div>
+
+        
+
+	</div>
+
+</div>
+</body>
+</html>
