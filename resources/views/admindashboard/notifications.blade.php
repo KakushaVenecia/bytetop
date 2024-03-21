9ced0f4cbee4f2e72dfeec20698f7472274ac6e6
Mairaj Shakeel Khan <220281081@aston.ac.uk>
2024-03-21 14:12:20 +0000

    added notification buttons

--- resources/views/admindashboard/notifications.blade.php
+++ resources/views/admindashboard/notifications.blade.php
@@ -1,11 +1,52 @@
 @extends('admindashboard.layout')
 @section('title', 'Notifications')
 @section('content')
-<div class="content">
-<div id="notifications">
-            <!-- Settings content -->
-            <h1>Notifications</h1>
-            <p>This is the settings content.</p> 
+<div class="buttons">
+        <button onclick="notifySuccess()">
+        Success
+        </button>
+        <button onclick="notifyError()">
+        Error
+        </button>
+        <button onclick="notifyInfo()">
+        Info
+        </button>
+</div>
+
+<div id="notification-area">
+        <div class="notification success">
+                This is demo success notification
+        </div>
+        <div class="notification error">
+                This is demo error notification
+        </div>
+        <div class="notification info">
+                This is demo info notification
         </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         @endsection
