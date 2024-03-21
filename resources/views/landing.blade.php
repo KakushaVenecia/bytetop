797728d8df422c64eda74cbe83b45aaca20676ab
Saikiran_SurineniLogin. <saikiransurineni616@gmail.com>
2024-03-21 15:03:26 +0000

    landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
   
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
     <title>Document</title>
 </head>
 
@@ -14,7 +14,7 @@
     @include('partials.navbar')
    
     {{-- <!-- <img class="slide" src="{{asset('images/header.webp')}}" alt="" srcset=""> --> --}}
-    {{-- <section class="hero">
+    {{-- <section class="hero"> --}}
     {{-- <link rel="preload" as="image" href="https://source.unsplash.com/300x360?0" />
     <link rel="preload" as="image" href="https://source.unsplash.com/300x360?1" />
     <link rel="preload" as="image" href="https://source.unsplash.com/300x360?2" /> --}}
@@ -26,47 +26,68 @@
       <a href="#" class="btn">Browse our cataloge</a>
     </div>
   </section> --}} 
-    <div class="container">
-        <h1>Product Catalogue</h1>
-        <div class="deals">
-            <div class="card">
-                <h3><b>Laptop Product Catalogue</b></h3>
-                <img class="card-img" src="{{ asset('images/Hp_Laptop.jpeg') }}" alt="Hp laptop">
-                <div class="more">
-                    <button  class="btn btn-add">View More</button>
-                </div>
-                </div>
-            <div class="card">
-                <h3><b>Computers Product Catalogue</b></h3>
-                <img class="card-img" src="{{ asset('images/computerimg.png') }}" alt="computerimg">
-                <div class="more">
-                    <button class="btn btn-add">View More</button>
-                </div>
-           
-                
-            </div>
-            <div class="card">
-                <h3><b>Accesories Product Catalogue</b></h3>
-                <img class="card-img" src="{{ asset('images/Keyboard.webp') }}" alt="keyboard">
-                <div class="more">
-                    <button class="btn btn-add">View More</button>
-                </div>
-                </div>
-            <div class="card">
-                <h3><b>Monitors Product Catalogue</b></h3>
-                <img class="card-img" src="{{ asset('images/Monitor.jpg') }}" alt="Monitor">
-                <div class="more">
-                    <button class="btn btn-add">View More</button>
-                </div>
-                </div>
-            <div class="card">
-                <h3><b>All in one Desktops Product Catalogue</b></h3>
-                <img class="card-img" src="{{ asset('images/all in one Desktop.AVIF') }}" alt="all in one Desktop">
-                <div class="more">
-                    <button class="btn btn-add">View More</button>
-                </div>        
-            </div>
-        </div>
+  
+  
+  <div class="container" id="productContainer">
+    <div class="product laptops">
+      <button class="arrow left">&lt;</button>
+      <div class="product-content">
+        <a href="laptops.html">
+          <h2>Laptops Catalogues</h2>
+          <p> Here you find the Best Laptops.</p>
+          <p> Click here to view more...</p>
+        </a>
+      </div>
+      <button class="arrow right">&gt;</button>
+    </div>
+    <div class="product computers">
+      <button class="arrow left">&lt;</button>
+      <div class="product-content">
+        <a href="computers.html">
+          <h2>Computers Catalogues</h2>
+          <p> Here you find the Best Computers.</p>
+          <p> Click here to view more...</p>
+        </a>
+      </div>
+      <button class="arrow right">&gt;</button>
+    </div>
+    <div class="product accessories">
+      <button class="arrow left">&lt;</button>
+      <div class="product-content">
+        <a href="accessories.html">
+          <h2>Accessories Catalogues</h2>
+          <p> Here you find the Best Accessories.</p>
+          <p> Click here to view more...</p>
+        </a>
+      </div>
+      <button class="arrow right">&gt;</button>
+    </div>
+    <div class="product monitors">
+      <button class="arrow left">&lt;</button>
+      <div class="product-content">
+        <a href="monitors.html">
+          <h2>Monitors Catalogues</h2>
+          <p> Here you find the Best Monitors.</p>
+          <p> Click here to view more...</p>
+        </a>
+      </div>
+      <button class="arrow right">&gt;</button>
+    </div>
+    <div class="product desktops">
+      <button class="arrow left">&lt;</button>
+      <div class="product-content">
+        <a href="desktops.html">
+          <h2>All in one Desktops Catalogues</h2>
+          <p> Here you find the Best All in one Desktops.</p>
+          <p> Click here to view more...</p>
+        </a>
+      </div>
+      <button class="arrow right">&gt;</button>
+    </div>
+  </div>
+
+
+
     </div>
     <h1>Popular Deals</h1>
     <div class="grid-container">
@@ -116,6 +137,7 @@
     @include('partials.footer')
     <script src="{{ asset('js/script.js') }}"></script>
     <script src="js/nav.js"></script>
+    <script src="js/landing.js"></script>
 </body>
 
 </html>
