4418b33bc319edc330c471593908af29038382d7
Saikiran_SurineniLogin. <saikiransurineni616@gmail.com>
2024-03-22 15:26:49 +0000

    forgotpwd is updated

--- resources/views/forgotpwd.blade.php
+++ resources/views/forgotpwd.blade.php
@@ -13,10 +13,14 @@
 </head>
 <body>
 
+
 <div class="container">
-  <h2>Forgot</h2>
-  <h3>Password</h3>
+<img src="/images/Bytetoplogo.png" alt="Logo">
+
+  <h2>Forgot Password</h2>
+  
   <hr class="white-line">
+  
 <div class="happen">
   <p>That's ok, It happens!</p>
 
